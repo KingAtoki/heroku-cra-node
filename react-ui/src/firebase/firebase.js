42ef258b5d0f7cced7cbdfc14c570f59d3fa84e3
Christopher Atoki <kingatoki@Christophers-MacBook-Air.local>
2018-04-19 16:58:34 -0400

    changes to add saras changes

--- react-ui/src/firebase/firebase.js
+++ react-ui/src/firebase/firebase.js
@@ -1,12 +1,20 @@
 import * as firebase from 'firebase';
 
+var admin = require("firebase-admin");
+
+
+admin.initializeApp({
+  credential: admin.credential.cert('firebase-adminsdk-ydvtl@trains-635b2.iam.gserviceaccount.com'),
+});
+
 const config = {
     apiKey: "AIzaSyDOY55XaKID0G9EuPr2ijexwe0_scPX1Hk",
     authDomain: "trains-635b2.firebaseapp.com",
     databaseURL: "https://trains-635b2.firebaseio.com",
     projectId: "trains-635b2",
     storageBucket: "trains-635b2.appspot.com",
-    messagingSenderId: "756876626177"
+    messagingSenderId: "756876626177",
+
 };
 
 
