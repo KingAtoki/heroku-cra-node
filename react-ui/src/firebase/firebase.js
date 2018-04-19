f9d182acbb426457538bc388e977149ede3c79ba
Christopher Atoki <kingatoki@Christophers-MacBook-Air.local>
2018-04-19 17:01:57 -0400

    changes to add saras changes

--- react-ui/src/firebase/firebase.js
+++ react-ui/src/firebase/firebase.js
@@ -1,31 +1,19 @@
 import * as firebase from 'firebase';
 
-var admin = require("firebase-admin");
-
-
-admin.initializeApp({
-  credential: admin.credential.cert('firebase-adminsdk-ydvtl@trains-635b2.iam.gserviceaccount.com'),
-});
-
 const config = {
-    apiKey: "AIzaSyDOY55XaKID0G9EuPr2ijexwe0_scPX1Hk",
-    authDomain: "trains-635b2.firebaseapp.com",
-    databaseURL: "https://trains-635b2.firebaseio.com",
-    projectId: "trains-635b2",
-    storageBucket: "trains-635b2.appspot.com",
-    messagingSenderId: "756876626177",
-
+  apiKey: 'AIzaSyDOY55XaKID0G9EuPr2ijexwe0_scPX1Hk',
+  authDomain: 'trains-635b2.firebaseapp.com',
+  databaseURL: 'https://trains-635b2.firebaseio.com',
+  projectId: 'trains-635b2',
+  storageBucket: 'trains-635b2.appspot.com',
+  messagingSenderId: '756876626177',
+  credential: admin.credential.cert(
+    'firebase-adminsdk-ydvtl@trains-635b2.iam.gserviceaccount.com'
+  )
 };
-
-
-if (!firebase.apps.length) {
-    firebase.initializeApp(config);
-}
+firebase.initializeApp(config);
 
 const db = firebase.database();
 const auth = firebase.auth();
 
-export {
-    db,
-    auth,
-};
+export { db, auth };
