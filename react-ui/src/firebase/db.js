11eb6b7f399c6b624035b7900a50826076bfc7c9
Christopher Atoki <kingatoki@Christophers-MacBook-Air.local>
2018-04-19 18:15:41 -0400

    changes to add saras changes

--- react-ui/src/firebase/db.js
+++ react-ui/src/firebase/db.js
@@ -2,12 +2,11 @@ import { db } from './firebase';
 
 // User API
 
-export const doCreateUser = (id, username, email, company ) =>
-    db.ref(`users/${id}`).set({
-        username,
-        email,
-        company,
-    });
+export const doCreateUser = (id, username, email, company) =>
+  db.ref(`users/${id}`).set({
+    username,
+    email,
+    company
+  });
 
-export const onceGetUsers = () =>
-    db.ref('users').once('value');
+export const onceGetUsers = () => db.ref('users').once('value');
