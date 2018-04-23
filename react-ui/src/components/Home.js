17846d47b361fd45d34f5a4ac28fbfde17ade019
Christopher Atoki <kingatoki@Christophers-MacBook-Air.local>
2018-04-23 12:25:28 -0400

    changes

--- react-ui/src/components/Home.js
+++ react-ui/src/components/Home.js
@@ -2,49 +2,46 @@ import React, { Component } from 'react';
 
 import './home.css';
 
-import withAuthorization from './withAuthorization';
-import { db } from '../firebase';
+//import withAuthorization from './withAuthorization';
 
 class HomePage extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            users: null,
-        };
-    }
+    this.state = {
+      users: null
+    };
+  }
 
-    componentDidMount() {
-        db.onceGetUsers().then(snapshot =>
-            this.setState(() => ({ users: snapshot.val() }))
-        );
-    }
+  // componentDidMount() {
+  //     db.onceGetUsers().then(snapshot =>
+  //         this.setState(() => ({ users: snapshot.val() }))
+  //     );
+  // }
 
-    render() {
-        const { users } = this.state;
+  render() {
+    const { users } = this.state;
 
-        return (
-            <div className="container">
-            <div className="home">
-
-                <h1>Home</h1>
-                <p>The Home Page is accessible by every signed in user.</p>
-                { !!users && <UserList users={users} /> }
-            </div>
-            </div>
-        );
-    }
+    return (
+      <div className="container">
+        <div className="home">
+          <h1>Home</h1>
+          <p>The Home Page is accessible by every signed in user.</p>
+          {!!users && <UserList users={users} />}
+        </div>
+      </div>
+    );
+  }
 }
-const UserList = ({ users }) =>
-    <div>
-        <h2>List of Usernames of Users</h2>
-        <p>(Saved on Sign Up in Firebase Database)</p>
+const UserList = ({ users }) => (
+  <div>
+    <h2>List of Usernames of Users</h2>
+    <p>(Saved on Sign Up in Firebase Database)</p>
 
-        {Object.keys(users).map(key =>
-            <div key={key}>{users[key].username}</div>
-        )}
-    </div>
+    {Object.keys(users).map(key => <div key={key}>{users[key].username}</div>)}
+  </div>
+);
 
-const authCondition = (authUser) => !!authUser;
+// const authCondition = (authUser) => !!authUser;
 
-export default withAuthorization(authCondition)(HomePage);
+// export default withAuthorization(authCondition)(HomePage);
